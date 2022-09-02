60961973f4cec7c1519eec7d0ed8cf8bf16ed9cb
sathya956 <knsathyamoorthybe@gmail.com>
2022-09-02 17:06:35 +0530

    02/09/2022

---
+++ src/org/geens/First.java
@@ -0,0 +1,13 @@
+package org.geens;
+
+public class First extends Second{
+
+	public void java() {
+		
+	}
+
+	public static void main(String[] args) {
+		
+	}
+}
+
---
+++ src/org/geens/Second.java
@@ -0,0 +1,14 @@
+package org.geens;
+
+public class Second {
+	
+	public static void a() {
+		int id=1000;
+		System.out.println(id);
+		
+	}
+public static void main(String[] args) {
+		
+	}
+
+}
